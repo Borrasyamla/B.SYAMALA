815925fb61973da594804b28e2906c765c9dbb4a
Borrasyamla <99325348+Borrasyamla@users.noreply.github.com>
2022-02-15 21:06:21 +0530

    Create Twelve.py

---
+++ Twelve.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+division=a//b
+print(division)
+floatdivision=a/b
+Input (stdin)
+
+4
+3
+Your Output (stdout)
+1
+1.33333333333
